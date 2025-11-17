3ab280369f1ad402c7be6e77c0234ce1465ba347
Axelle Mondoyo <axellemondoyo2@gmail.com>
2025-11-17 10:00:00 +0800

    Added division

---
+++ math.js
@@ -0,0 +1,22 @@
+
+
+function sum(num1, num2){
+    sum = num1 + num2;
+    console.log(sum);
+}
+function diff(num1, num2){
+    diff = num1 - num2;
+    console.log(diff);
+}
+function prod(num1, num2){
+    prod = num1 * num2;
+    console.log(prod);
+}
+function quot(num1, num2){
+    quot = num1 / num2;
+    console.log(quot);
+}
+sum(2,3);
+diff(5,3);
+prod(11, 5);
+quot(6,3);
