1e76fdcb903edf877c9125f0bb06f748e7252b9e
Axelle Monmdoyo <axellemondoyo2@gmail.com>
2025-11-20 12:00:00 +0800

    Update files (Nov 20, 2025)

---
+++ while-loop.js
@@ -0,0 +1,42 @@
+// console.log("Descending");
+// i = 5;
+
+// while (i <= 1){
+//     console.log (i);
+//     i--;
+
+// }
+// console.log("Ascending");
+// x = 1;
+
+// while (x <=5){
+//     console.log (x);
+//     x++;
+
+// }
+// output = " ";
+// y=1;
+// while (y<=5){
+//     output = " ";
+//  y++;
+
+// }
+
+// arr=[1,2,3];
+// output + " ";
+// index =0;
+
+// while (index <= arr.length -1){
+//     output += arr[index] + " ";
+//     index++;
+// }
+// console.log(output);
+
+console.log ("Reverse");
+1=5;
+
+while (i >=1){
+    console.log (i);
+    i--;
+    
+}
