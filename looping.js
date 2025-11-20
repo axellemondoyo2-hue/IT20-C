83649638abf2d541c0d860404d481414ba26cc83
Axelle Mondoyo <axellemondoyo2@gmail.com>
2025-11-20 10:00:00 +0800

    Added for loop

---
+++ looping.js
@@ -0,0 +1,33 @@
+
+
+// //FOR LOOP
+// output = " ";
+// for(i = -10; i<=5; i++ ){
+//     // console.log(i);
+//     output += i + " ";
+    
+// }
+
+// console.log(output);
+
+//ARRAYS
+// output = " ";
+// arr = [1,2,3]
+
+// for (index=0; index<arr.length; index++){
+//     // console.log(arr[i]);
+//     output += arr[index] + " ";
+
+// }
+// console.log(output);
+
+output = " ";
+arr = [1,2,3]
+console.log ("Array Length: " + arr.length);
+
+for (index= arr.length -1; index>=0; index--){
+    // console.log(arr[index]);
+    output += arr[index] + " ";
+
+}
+console.log(output);
